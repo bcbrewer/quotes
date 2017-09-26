9a6219f30493e82c76ef0abbaae46f7cbc2d79ac
Ben Brewer <bcbrewer777@gmail.com>
2017-09-25 21:22:57 -0400

    Update yarn

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,16 +1,34 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
-                    <div class="panel-body">
-                        I'm an example component!
-                    </div>
-                </div>
+    <div class="box">
+        <article class="media">
+            <div class="media-left">
+                <figure class="image is-64x64">
+                    <img src="http://bulma.io/images/placeholders/128x128.png" alt="Image">
+                </figure>
             </div>
-        </div>
+            <div class="media-content">
+                <div class="content">
+                    <p>
+                        <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                        <br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+                    </p>
+                </div>
+                <nav class="level is-mobile">
+                    <div class="level-left">
+                        <a class="level-item">
+                            <span class="icon is-small"><i class="fa fa-reply"></i></span>
+                        </a>
+                        <a class="level-item">
+                            <span class="icon is-small"><i class="fa fa-retweet"></i></span>
+                        </a>
+                        <a class="level-item">
+                            <span class="icon is-small"><i class="fa fa-heart"></i></span>
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        </article>
     </div>
 </template>
 
--- resources/assets/js/components/draggable-example.vue
+++ resources/assets/js/components/draggable-example.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Draggable Component</div>
-
-                    <draggable v-model="myArray" :options="{group:'people'}" @start="drag=true" @end="drag=false">
-                        <div v-for="element in myArray">{{element}}</div>
-                    </draggable>
-                </div>
+    <nav class="level">
+        <div class="container">
+            <div class="level-item has-text-centered">
+                <p class="heading">Draggable Component</p>
             </div>
+            <draggable v-model="myArray" :options="{group:'people'}" @start="drag=true" @end="drag=false">
+                <div v-for="element in myArray">
+                    <div class="level-item has-text-centered">
+                        <p class="title">
+                            {{element}}
+                        </p>
+                    </div>
+                </div>
+            </draggable>
         </div>
-    </div>
+    </nav>
+
 </template>
 
 <script>
